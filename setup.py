c4ec1a48ce59f510d7f698107ac3b0cbbd2ee986
Shawn Wallis <shawn.p.wallis@gmail.com>
2017-05-22 20:12:48 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+
+setup(name='shython',
+      version='0.1.0',
+      url='https://github.com/shawalli/shython',
+      author='Shawn Wallis',
+      author_email='shawn.p.wallis@gmail.com',
+      description='Python interpreter with some extra usability features',
+      scripts=['bin/shython'],
+)
